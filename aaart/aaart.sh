eda71ddb0163bb88cc5235bee06909aed6b825f4
tom fleet <tomfleet@gmail.com>
2024-10-10 03:29:58 +0100

    go to bedddd

--- aaart/aaart.sh
+++ aaart/aaart.sh
@@ -76,7 +76,7 @@ fi
 
 #jesus that was an ordeal.
 
-jp2a --colors --width=80  albumart.jpg > asciiart.txt
+jp2a --colors --width=64 --fill --clear -b albumart.jpg > asciiart.txt
 if [ $? -ne 0 ]; then
     echo "jp2a command failed"
     exit 1
--- aaoutput/aaoutput.sh
+++ aaoutput/aaoutput.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+
+
+DEBUG=0
+
+# Parse command-line arguments
+while getopts "d" opt; do
+  case $opt in
+    d)
+      DEBUG=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Function to print debug messages
+debug_echo() {
+  if [ $DEBUG -eq 1 ]; then
+    echo "$1"
+  fi
+}
+
+
 #check if demo dir exists and if so rm it
 if [ -d "demo" ]; then
     rm -rfd demo
@@ -16,19 +42,19 @@ fi
 
 url=$1
 
-echo "Captured URL: $url"
+debug_echo "Captured URL: $url"
 
 #sudo dos2unix ../aaart/aaart.sh 
 
 # Call the script and print the output
 
 pwd=$(pwd)
-echo "Current directory for art: $pwd"
+debug_echo "Current directory for art: $pwd"
 
 call_aaart() {
     ../../aaart/aaart.sh "$1"
     if [ $? -ne 0 ]; then
-        echo "yt-dlp [tracklist] failed"
+        debug_echo "yt-dlp [tracklist] failed"
         exit 1
     fi
 }
@@ -36,7 +62,7 @@ call_aaart() {
 call_aaart "$url"
 
 if [ $? -ne 0 ]; then
-    echo "yt-dlp [tracklist] failed"
+    debug_echo "yt-dlp [tracklist] failed"
     exit 1
 fi
 
@@ -44,26 +70,26 @@ fi
 
 
 pwd=$(pwd)
-echo "Current directory after art: $pwd"
+debug_echo "Current directory after art: $pwd"
 
 
 call_aainfo() {
     ../../aainfo/aainfo.sh "$1"
     if [ $? -ne 0 ]; then
-        echo "aainfo.sh failed"
+        debug_echo "aainfo.sh failed"
         exit 1
     fi
 }
 
 call_aainfo "$url"
 if [ $? -ne 0 ]; then
-    echo "aainfo.sh failed"
+    debug_echo "aainfo.sh failed"
     exit 1
 fi
 
 
 pwd=$(pwd)
-echo "Current directory after info: $pwd"
+debug_echo "Current directory after info: $pwd"
 dir=$pwd
 #look inside the most recently modified directory for a json file
 json_file=$(find "$dir" -name "*.json" | head -n 1)
